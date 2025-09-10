6945dc45f18b2b71cb4d395a56e42613e720d59e
Helena <144626169+helenafilemon@users.noreply.github.com>
2025-09-10 14:57:02 -0300

    Update opLogicoAND.v

--- operadores logicos/opLogicoAND.v
+++ operadores logicos/opLogicoAND.v
@@ -1,11 +1,10 @@
-module opLogicoAND(W, X, Y, Z, A, B, C, D, E, F, G, H);
-	input A, B, C, D;
-	input E, F, G, H;
-	output W, X, Y, Z;
+module opLogicoAND(S, A, B);
+	input [3:0] A, B;
+	output [3:0] S;
 	
-	and And0(W, A, E);
-	and And1(X, B, F);
-	and And2(Y, C, G);
-	and And3(Z, D, H);
+	and And0(S[0], A[0], B[0]);
+	and And1(S[1], A[1], B[1]);
+	and And2(S[2], A[2], B[2]);
+	and And3(S[3], A[3], B[3]);
 	
 endmodule
