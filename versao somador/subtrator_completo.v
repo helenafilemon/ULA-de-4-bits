e71aab78ed76967377a3a65bbb3fd6553826f54d
Helena <144626169+helenafilemon@users.noreply.github.com>
2025-09-10 14:45:59 -0300

    Update subtrator_completo.v

--- versao somador/subtrator_completo.v
+++ versao somador/subtrator_completo.v
@@ -1,19 +1,15 @@
 module subtrator_completo(Diferenca, Bout, A, B, Bin );
-	input A;
-	input B;
-	input Bin;
-	output Diferenca;
-	output Bout;
+	input A, B, Bin;
+	output Diferenca, Bout;
 
-	wire w1, w2, w3, w4;
-
+	wire w1, w2, w3, w4, w5;
 	xor (w1, A, B);
 	xor (Diferenca, w1, Bin);
 
 	not (w2, A);
+	not (w5,w1);
 	and (w3, w2, B);
-	and (w4, w2, Bin);
-	and (w5, B, Bin); 
-	or (Bout, w3, w4, w5);
+	and (w4, w5, Bin);
+	or (Bout, w3, w4);
 
 endmodule
