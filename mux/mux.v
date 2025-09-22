5d8618b5a68ee4534438c2b5ee9bc391c0b31296
Davi Queiroz <daviengcomp@gmail.com>
2025-09-22 10:46:59 -0300

    Update mux.v

--- mux/mux.v
+++ mux/mux.v
@@ -14,19 +14,19 @@ module mux (Y, I0, I1, I2, I3, I4, I5, I6, I7, S0, S1, S2);
 	wire and_out0, and_out1, and_out2, and_out3, and_out4, and_out5, and_out6, and_out7;
 	
 	// instanciando as 7 portas AND para cada combinação de seletor
-	// 000 (I0)
+	// 000 (I0) / DIV
 	and And0(and_out0, I0, not_S2, not_S1, not_S0);
-	// 001 (I1)
+	// 001 (I1) /AND
 	and And1(and_out1, I1, not_S2, not_S1, S0);
-	// 010 (I2)
+	// 010 (I2) /OR
 	and And2(and_out2, I2, not_S2, S1, not_S0);
-	// 011 (I3)
+	// 011 (I3) /XOR
 	and And3(and_out3, I3, not_S2, S1, S0);
-	// 100 (I4)
+	// 100 (I4) /ADD
 	and And4(and_out4, I4, S2, not_S1, not_S0);
-	// 101 (I5)
+	// 101 (I5) /SUB
 	and And5(and_out5, I5, S2, not_S1, S0);
-	// 110 (I6)
+	// 110 (I6) /MULT
 	and And6(and_out6, I6, S2, S1, not_S0);
 	// 111 (I7) 
 	and And7(and_out7, I7, S2, S1, S0);
