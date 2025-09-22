31a97e80f9bdbbd14c747a0f17d7306ca6307b32
Davi Queiroz <daviengcomp@gmail.com>
2025-09-22 11:04:38 -0300

    Update subtrator_4bits.v

--- subtrator/subtrator_4bits.v
+++ subtrator/subtrator_4bits.v
@@ -6,32 +6,32 @@ module subtrator_4bits(S, Bout, A, B);
     wire b0, b1, b2;
 
     subtrator_completo sub0 (
-        .A(A[0]),
-        .B(B[0]),
+        .A(B[0]),
+        .B(A[0]),
         .Bin(1'b0),
         .Diferenca(S[0]),
         .Bout(b0)
     );
 
     subtrator_completo sub1 (
-        .A(A[1]),
-        .B(B[1]),
+        .A(B[1]),
+        .B(A[1]),
         .Bin(b0),
         .Diferenca(S[1]),
         .Bout(b1)
     );
 
     subtrator_completo sub2 (
-        .A(A[2]),
-        .B(B[2]),
+        .A(B[2]),
+        .B(A[2]),
         .Bin(b1),
         .Diferenca(S[2]),
         .Bout(b2)
     );
 
     subtrator_completo sub3 (
-        .A(A[3]),
-        .B(B[3]),
+        .A(B[3]),
+        .B(A[3]),
         .Bin(b2),
         .Diferenca(S[3]),
         .Bout(Bout)
