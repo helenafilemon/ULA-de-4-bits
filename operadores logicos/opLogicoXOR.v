b338b99c1b380b6c1415bd45e3587ee8ad02775c
Davi Queiroz <daviengcomp@gmail.com>
2025-09-24 11:08:36 -0300

    Update opLogicoXOR.v

--- operadores logicos/opLogicoXOR.v
+++ operadores logicos/opLogicoXOR.v
@@ -1,10 +1,10 @@
-module opLogicoXOR(S, A, B);
+	module opLogicoXOR(S, A, B);
 	input [3:0] A, B;
 	output [3:0] S;
 	
-	and Xor0(S[0], A[0], B[0]);
-	and Xor1(S[1], A[1], B[1]);
-	and Xor2(S[2], A[2], B[2]);
-	and Xor3(S[3], A[3], B[3]);
+	xor Xor0(S[0], A[0], B[0]);
+	xor Xor1(S[1], A[1], B[1]);
+	xor Xor2(S[2], A[2], B[2]);
+	xor Xor3(S[3], A[3], B[3]);
 	
 endmodule
